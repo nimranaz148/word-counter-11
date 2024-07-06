ed14390dc924219384151c0ed04a6c3f0e97985e
nimranaz148 <nimranaz148@gmail.com>
2024-07-06 08:19:53 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+let message = await inquirer.prompt({
+    name: "ans",
+    type: "input",
+    message: "enter your message"
+});
+let words = message.ans.trim().split(" ");
+console.log(`We have ${words.length} words in this sentence`);
+let char_count = words.join("");
+console.log(`We have ${char_count.length} characters in this sentence`);
